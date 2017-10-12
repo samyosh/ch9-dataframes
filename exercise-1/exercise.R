27006723049606656672dded308b5e3f3ca8abaa
Sam Yoshikawa <samyosh@uw.edu>
2017-10-12 14:17:25 -0700

    Added points, scores, teams

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -5,19 +5,22 @@
                            # http://www.footballdb.com/teams/nfl/seattle-seahawks/results
                            # here 'Result' displays scores, 'L' stands for loss, 'W' for win
 
+points.for <- c(9, 12, 27, 46, 16)
 # Create a vector of the number of points the Seahawks have allowed to be scored against them in the first 5 games
-
+points.against <- c(17, 9, 27, 18, 10)
 
 # Combine your two vectors into a dataframe
-
+#score$new <- score$points.against - score$points.for 
+scores <- data.frame(points.for, points.against)
 
 # Create a new column "diff" that is the difference in points
-
+scores <- data.frame(scores, "diff" = points.for - points.against)
 
 # Create a new column "won" which is TRUE if the Seahawks won
-
+scores <- data.frame(scores, "won" = points.for > points.against)
 
 # Create a vector of the opponents
-
+opps <- c("Packers", "Fortyniners", "Titans", "Colts", "Rams")
 
 # Add the vector of opponents into the data frame
+scores <- data.frame(scores, opps)
